280c255862eb196252b4ddeae8e60770cc8efa91
Rishi_official432-hub <rishiwork432@gmail.com>
2025-08-04 22:02:17 +0530

    Update SecretLink.tsx

--- components/SecretLink.tsx
+++ components/SecretLink.tsx
@@ -4,7 +4,7 @@ export default function SecretLink() {
     return (
         <div className="mt-8 text-center">
             <a
-                href="https://boisterous-puppy-e52c9f.netlify.app/"
+                href="https://new-music-player-rose.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block bg-violet-600 text-white font-semibold py-3 px-8 rounded-lg transition-all duration-300 hover:scale-105 hover:bg-violet-500 focus:outline-none focus:ring-2 focus:ring-violet-400 focus:ring-opacity-75"
